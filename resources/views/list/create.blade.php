912fbd7ff99be154069002bfcedfd58a4b0085af
Domingo Brandt <brandtyet02@gmail.com>
2018-10-14 19:53:51 -0400

    Requests

--- resources/views/list/create.blade.php
+++ resources/views/list/create.blade.php
@@ -3,33 +3,13 @@
 @section('title', 'List create')
 
 @section('content')
-<div class="container">
-    <div class="form-group">
-        <label for="">nombre</label>
-        <input type="text" class="form-control">
-    </div>
-    <button type="button" class="btn btn-primary">guardar</button>
-    </div>
+<form class="form-group" method="POST" action="/list">
+    @csrf
+        <div class="form-group">
+            <label for="">nombre</label>
+            <input type="text" name="name" class="form-control">
+        </div>
+        <button type="submit" class="btn btn-primary">guardar</button>
+</form>
+
 @endsection
-{{--
-<!DOCTYPE html>
-<html>
-<head>
-    <meta charset="utf-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>Page Title</title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" type="text/css" media="screen" href="main.css" />
-    <script src="main.js"></script>
-</head>
-<body>
-<div class="container">
-<div class="form-group">
-    <label for="">nombre</label>
-    <input type="text" class="form-control">
-</div>
-<button type="button" class="btn btn-primary">guardar</button>
-</div>
-</body>
-</html>--}}
