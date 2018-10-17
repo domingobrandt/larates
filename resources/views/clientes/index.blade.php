de7743682040f983bea54ea32196e91fa3243840
Domingo Brandt <brandtyet02@gmail.com>
2018-10-17 00:39:02 -0400

    view client

--- resources/views/clientes/index.blade.php
+++ resources/views/clientes/index.blade.php
@@ -7,8 +7,9 @@
 <div class="row">
     @foreach ($clientes as $cliente)
         <div class="col-sm">
-            <div class="card" style="width: 18rem;">
-                    <img class="card-img-top" src="images/{{$cliente->avatar}}" alt="{{$cliente->avatar}}">
+            <div class="card text-center" style="width: 18rem; margin-top: 50px;">
+                    <img style=" margin-top: 25px;
+                     width: 60%; background-color: #efefef;" class="card-img-top rounded-circle mx-auto d-block"  src="images/{{$cliente->avatar}}" alt="{{$cliente->avatar}}">
                 <div class="card-body">
                     <h5 class="card-title">{{$cliente->name}}</h5>
                     <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
