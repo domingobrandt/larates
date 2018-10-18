aff241b4a7f7aa65fe3282a1d0eb80e14c64d2b5
Domingo Brandt <brandtyet02@gmail.com>
2018-10-18 12:17:37 -0400

    subViews

--- resources/views/clientes/edit.blade.php
+++ resources/views/clientes/edit.blade.php
@@ -3,24 +3,13 @@
 @section('title', 'cliente Edit')
 
 @section('content')
+
 {!! Form::model($cliente, ['route' => ['cliente.update', $cliente], 'method' => 'PUT', 'files' => true])!!}
 
-<div class="form-group">
-    {!!Form::label('name','Nombre')!!}
-    {!!Form::text('name',null,['class'=>'form-control'])!!}
-</div>
+@include('clientes.form')
 
- <div class="form-group">
-    {!!Form::label('slug','Slug')!!}
-    {!!Form::text('slug',null,['class'=>'form-control'])!!}
- </div>
+{!! Form::submit('Actualizar', ['class' => 'btn btn-primary']) !!}
+{!! Form::close() !!}
 
-<div class="form-group">
-    {!!Form::label('avatar','Avatar')!!}
-    {!!Form::file('avatar')!!}
-</div>
-
-	{!! Form::submit('Actualizar', ['class' => 'btn btn-primary']) !!}
-	{!! Form::close() !!}
 @endsection
 
