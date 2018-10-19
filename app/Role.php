c458bc699338fb17c074a243b4bd4f05df3f8b4b
Domingo Brandt <brandtyet02@gmail.com>
2018-10-19 10:34:48 -0400

    Roles&UserModelMigrations

---
+++ app/Role.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Laratest;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Role extends Model
+{
+    public function roles(){
+        return $this->belongsToMany('Laratest\Role');
+    }
+}
