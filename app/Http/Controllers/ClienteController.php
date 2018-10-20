a3d1e5b17136ca9f0190731ac6b6eaef80c2fe4f
Domingo Brandt <brandtyet02@gmail.com>
2018-10-19 21:57:36 -0400

    RoleUser implementation

--- app/Http/Controllers/ClienteController.php
+++ app/Http/Controllers/ClienteController.php
@@ -12,8 +12,10 @@ class ClienteController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function index(Request $request)
     {
+        $request->user()->authorizeRoles(['admin','user']);
+
         $clientes = Cliente::all();
 
         return view('clientes.index', compact('clientes'));
