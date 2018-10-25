aa919b5e7e411fa83e6b97b81c85a88de73eb321
Domingo Brandt <brandtyet02@gmail.com>
2018-10-24 22:50:35 -0400

    nametesting

--- routes/web.php
+++ routes/web.php
@@ -14,13 +14,15 @@
 //Route::resource('cliente', 'ClienteController');
 //Route::get('/cliente', 'ClienteController@index')->name('cliente');
 
-
+Route::get('/', function () {
+    return view('welcome');
+});
 
 Auth::routes();
 
 
 Auth::routes();
 
-Route::get('/home', 'HomeController');
+//Route::get('/home', 'HomeController@index')->name('home');
 
 Route::resource('cliente', 'ClienteController');
