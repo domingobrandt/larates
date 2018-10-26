46fe1b814062a1cfb569c71d27ee619997f6bb8e
Domingo Brandt <brandtyet02@gmail.com>
2018-10-26 18:55:23 -0400

    fix User

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -15,7 +15,7 @@ class UserController extends Controller
     public function index(Request $request)
     {
         $users = User::all();
-        return View::make('user')->with('User',$users);
+        return view('users.index', compact('users'));
  
 
    }
