bc45e9e39895f4ad308a640fb54fc4fcafd92fd5
Domingo Brandt <brandtyet02@gmail.com>
2018-11-14 20:41:03 -0400

    cliente controller

--- app/Http/Controllers/ClienteController.php
+++ app/Http/Controllers/ClienteController.php
@@ -43,7 +43,8 @@ class ClienteController extends Controller
 
     public function create(Request $request)
     {
-        if( Auth::user()->rol =='admin' ) //se valida el tipo de usuario
+        
+        if( user()->authorizeRoles =='admin' ) //se valida el tipo de usuario
         return redirect('cliente.create');
     else
         return redirect('cliente.index');
